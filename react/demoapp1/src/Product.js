1cb25fcbe6735bfc870c4020734fabb9300ec53a
abhishek <111995abhishek@gmail.com>
2021-03-15 02:28:23 +0530

    added mounting method such as componentDidMount(),componentDidUpdate(),getDerivedStateFromProps(),shouldComponentUpdate()

--- react/demoapp1/src/Product.js
+++ react/demoapp1/src/Product.js
@@ -1,10 +1,31 @@
 import React,{Component} from 'react';
 
 class Product extends Component{
+    constructor(props){
+        super(props);
+        this.state={productid:'',qty:0,isCart:true} 
+    }
+
+    addToCart = (pid) => (
+        this.setState((state) =>(
+            { productid:pid, qty:state.qty + 1 }
+        )
+    ))
+
+    removeCart = () =>(
+        this.setState({isCart:false})
+    )
+
+
     render(){
         return(
             <div>
-                <Cart/>
+                <button onClick = {()=>this.addToCart(1)}>add to cart</button>
+                <button onClick = {()=>this.addToCart(2)}>add to cart</button>
+                <button onClick = {()=>this.addToCart(3)}>add to cart</button>
+                {this.state.isCart && <Cart productid={this.state.productid} qty={this.state.qty}/>}
+                {!this.state.isCart && <h2>cart removed</h2>}
+                <button onClick={this.removeCart}>remove cart</button>
             </div>
         );
     }
@@ -14,7 +35,7 @@ class Cart extends Component{
     constructor(props){
         super(props);
         this.state = {
-            qty:0
+            qty:this.props.qty
         }
     }
 
@@ -22,21 +43,43 @@ class Cart extends Component{
         console.log('executes after component render');
     }
 
-    UpdateQty = () =>(
+    /*UpdateQty = () =>(
         this.setState((state)=>(
             {qty:state.qty + 1}
         ))
-    )
+    )*/
+
+    static getDerivedStateFromProps(props,state){
+        if(props.qty !== state.qty) {
+            return {qty:props.qty}
+        }
+        return null;
+    }
+
+    shouldComponentUpdate(nextProps,nextState){
+        if(this.props.qty !== nextProps.qty){
+            console.log("should component updated");
+            return true;
+        }
+        return false;
+    }
 
     componentDidUpdate(prevProps,prevState){
-        console.log('component updated');
+        if(this.props.productid !== prevProps.productid){
+            console.log('component updated');
+        }
+        
+    }
+
+    componentWillUnmount(){
+        console.log("component destroyed");
     }
 
     render(){
         return(
             <div>
                 <h2>cart items {this.state.qty}</h2>
-                <button onClick={this.UpdateQty}>add to cart</button>
+                
             </div>
         );
     }
