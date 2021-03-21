8d3fbba7b7edb9254b6d01f8a580d37e7ac6c9b7
abhishek <111995abhishek@gmail.com>
2021-03-22 02:48:40 +0530

    created new project  called usercard and fetched all the detail from the user from a url of a json file

---
+++ react/usercard/src/Users.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import cartoon from './cartoon.jpg';
+
+
+
+class Users extends Component {
+
+    constructor() {
+        super();
+        this.state = { users: [] };
+    }
+    render() {
+        return (
+            <div style={{padding:"20px",background:"black"}}>
+                {this.state.users.map((user) => 
+                <div className="card" style = {{width: "18rem",border:"2px solid black",margin:"10px",background:"pink"}} key={user.id}>
+                <img src={cartoon} className="card-img-top" alt="mycartoon" width="100px"/>
+                <div className="card-body">
+                  <div >  
+                  <h5 className="card-title">{user.name}</h5>
+                  <p className="card-text"><b>Email: </b>{user.email}</p>
+                  <p className="card-text"><b>Username: </b>{user.username}</p>
+                  <p className="card-text"><b>Address: </b>{user.address.street},{user.address.suite},{user.address.city},{user.address.zipcode}</p>
+                  {/* <p className="card-text">{user.address.street}</p>
+                  <p className="card-text">{user.address.suite}</p>
+                  <p className="card-text">{user.address.city}</p>
+                  <p className="card-text">{user.address.zipcode}</p> */}
+                  <p className="card-text">{user.company.catchPhrase}</p>
+                  
+                  </div>
+                </div>
+              </div>)}
+                    
+            </div>
+                );
+    }
+
+    componentDidMount(){
+                    fetch(' https://jsonplaceholder.typicode.com/users')
+                        .then(response => response.json())
+                        .then(items => this.setState({ 'users': items }));
+    }
+}
+
+export default Users;
