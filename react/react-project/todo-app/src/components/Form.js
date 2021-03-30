2ee26803abde6cdbe3dcf9c649915cc1694f22d3
abhishek <111995abhishek@gmail.com>
2021-03-30 15:01:32 +0530

    add new functionality in todo app to add  to the database

--- react/react-project/todo-app/src/components/Form.js
+++ react/react-project/todo-app/src/components/Form.js
@@ -1,16 +1,26 @@
 import React,{useState} from 'react';
+import firebase from '../firebase';
 
-const Form = () => {
-    const[title,setTitle]= useState('');
+function Form  () {
+    const[title, setTitle] = useState('');
 
     const handleOnChange=(event)=>{
-        this.setTitle(event.target.value);
+        setTitle(event.target.value);
         
+    };
+
+    const createTodo=()=>{
+        const todoRef=firebase.database().ref('Todo');
+        const todo={
+            title,
+            complete:false,
+        };
+        todoRef.push(todo);
     }
     return (
         <div>
-            <input type="text" onChange={handleOnChange}/>
-            <button>add todo</button>
+            <input type="text" onChange={handleOnChange} value={title}/>
+            <button onClick={createTodo}>add todo</button>
         </div>
     );
 };
