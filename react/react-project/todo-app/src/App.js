36bec7f792938a8fbe689a25e6cbbca9557d9058
abhishek <111995abhishek@gmail.com>
2021-05-05 09:58:24 +0530

    able to add and fetch data from database and display into the ui

--- react/react-project/todo-app/src/App.js
+++ react/react-project/todo-app/src/App.js
@@ -7,16 +7,16 @@ import Todo from './todo';
 function App() {
 
   const [todo, setTodo] = useState([]);
-  const [title,setTitle] = useState('');
-  const [date,setDate] = useState('');
-  const [time,setTime] = useState('');
-  
-  
-  
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+
+
+
 
   useEffect(() => {
     db.collection("todo").onSnapshot(snapshot => {
-      setTodo(snapshot.docs.map(doc => ({id:doc.id,todos:doc.data().todos})))
+      setTodo(snapshot.docs.map(doc => doc.data()))
     })
   }, []);
 
@@ -41,7 +41,7 @@ function App() {
     <div className="App">
 
       <h1>TODO LIST</h1>
-      
+
       <form >
         <input type="text"
           placeholder="enter a title..."
@@ -57,7 +57,7 @@ function App() {
           placeholder="enter the time..."
           onChange={event => setTime(event.target.value)}
           value={time} />
-          
+
 
         <button type="submit" onClick={addTodo}>Add Todo</button>
 
@@ -66,12 +66,12 @@ function App() {
 
       {
         todo.map((todos) => (
-          <Todo  title={todos.title} date={todos.date} time={todos.time}   />
+          <Todo title={todos.title} date={todos.date} time={todos.time} />
         ))
       }
-      
 
-      
+
+
     </div>
   );
 }
--- react/react-project/todo-app/src/todo.js
+++ react/react-project/todo-app/src/todo.js
@@ -1,16 +1,16 @@
 import './todo.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { db } from './firebase';
 
-function Todo({title,date,time}){
-    const [complete,setComplete] = useState(false);
+function Todo(props) {
+    const [complete, setComplete] = useState(false);
 
-    
-    return(
+
+    return (
         <div className="task">
-            <h3>{title}</h3>
-            <h3>{date}</h3>
-            <h3>{time}</h3>
+            <h3>{props.title}</h3>
+            <h3>{props.date}</h3>
+            <h3>{props.time}</h3>
             {/* <input className="todo-complete" key={id} type="checkbox"   onClick={markComplete}/> */}
             <button>completed</button>
             <button>delete</button>
